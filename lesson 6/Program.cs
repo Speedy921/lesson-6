682fc2c32da539a9db8d99febf62be00b605aab0
Artem Viatkin <newyorkbox47@gmail.com>
2022-12-10 15:07:49 +0200

    Добавьте файлы проекта.

---
+++ lesson 6/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//члены сжатые до выражений
+//синтаксический сахар
+static int Add(int x, int y) => x + y;
+
+
+//статическая локасльная функция 
+static int AddWrapperWithStatic(int x, int y)
+{
+    //выполнение проверки достоверности
+    return Add(x, y);
+
+    static int Add(int x, int y)
+    { return x + y; }
+}
